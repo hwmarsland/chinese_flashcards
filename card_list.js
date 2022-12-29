45180c479f2235a251c3bbbf9f160f2c087c5763
hwmarsland <hwmarsland@wm.edu>
2022-12-29 17:09:50 -0500

    Simple starting database with example data

---
+++ card_list.js
@@ -0,0 +1,12 @@
+const cards = [
+    ['搬','bān','to move'],
+    ['但是','dànshì','but'],
+    ['必须','bìxū','must'],
+    ['房租','fángzū','to rent'],
+    ['楼上','lóushàng','upstairs'],
+    ['马上','mǎshàng','immediately'],
+    ['房东','fángdōng','landlord'],
+    ['房客','fángkè','tentant'],
+    ['以后','yǐhòu','after'],
+    ['好像','hǎoxiàng','to seem like'],
+]
