dab40f1c86442f105ba704ae8c60ea06eb945d1c
hwmarsland <hwmarsland@wm.edu>
2023-01-07 20:32:03 -0500

    Card flip operations beginning to be implemented

--- card_list.js
+++ card_list.js
@@ -1,14 +1,14 @@
 const cards = [
-    ['搬','bān','to move'],
-    ['但是','dànshì','but'],
-    ['必须','bìxū','must'],
-    ['房租','fángzū','to rent'],
-    ['楼上','lóushàng','upstairs'],
-    ['马上','mǎshàng','immediately'],
-    ['房东','fángdōng','landlord'],
-    ['房客','fángkè','tentant'],
-    ['以后','yǐhòu','after'],
-    ['好像','hǎoxiàng','to seem like'],
+    {characters: '搬', pinyin: 'bān', english: 'to move'},
+    {characters: '但是', pinyin: 'dànshì', english: 'but'},
+    {characters: '必须', pinyin: 'bìxū', english: 'must'},
+    {characters: '房租', pinyin: 'fángzū', english: 'to rent'},
+    {characters: '楼上', pinyin: 'lóushàng', english: 'upstairs'},
+    {characters: '马上', pinyin: 'mǎshàng', english: 'immediately'},
+    {characters: '房东', pinyin: 'fángdōng', english: 'landlord'},
+    {characters: '房客', pinyin: 'fángkè', english: 'tentant'},
+    {characters: '以后', pinyin: 'yǐhòu', english: 'after'},
+    {characters: '好像', pinyin: 'hǎoxiàng', english: 'to seem like'},
 ]
 
 // Keeps track of the card being displayed
@@ -16,13 +16,28 @@ var cardIndexPointer = 0;
 // Keeps track of the deck currently in use
 var cardDeck = cards;
 
-function nextCard(){
+export function nextCard(){
     cardIndexPointer += 1;
-    return (cardIndexPointer%cardDeck.length())
+    return (cardIndexPointer%cardDeck.length());
 };
 
-function previousCard(){
+export function previousCard(){
     cardIndexPointer -= 1;
     cardIndexPointer += cardDeck.length();
-    return (cardIndexPointer%cardDeck.length())
+    return (cardIndexPointer%cardDeck.length());
+};
+
+export function flipCard(cardSide){
+    if (cardSide == "Characters"){
+        return(cardDeck[cardIndexPointer].characters);
+    }
+    else if (cardSide == "Pinyin"){
+        return(cardDeck[cardIndexPointer].pinyin);
+    }
+    else if (cardSide == "English"){
+        return(cardDeck[cardIndexPointer].english);
+    }
+    else {
+        console.log("SOMETHING IS WRONG: You shouldnt be here")
+    }
 };
--- card_viewer.js
+++ card_viewer.js
@@ -1,21 +1,39 @@
+// import { nextCard } from "./card_list";
+// import { previousCard } from "./card_list";
+import { flipCard } from "./card_list";
+
 const upArrow = document.getElementById("upArrow")
 
 upArrow.addEventListener('click', function handleUpClick() {
     console.log('Up arrow button clicked');
     //Handling the visual changes
-    if (document.getElementById("curTextType") == "Characters") {
-
+    if (document.getElementById("curTextType").innerHTML == "Characters") {
+        //Setting button text
+        document.getElementById("upTextType").innerHTML = "▲ English ▲";
+        document.getElementById("curTextType").innerHTML = "Pinyin";
+        document.getElementById("downTextType").innerHTML = "▼ Characters ▼";
+        //Setting flashcard text
+        document.getElementById("cardText").innerHTML = upCard("Pinyin");
     }
-    else if (document.getElementById("curTextType") == "Characters") {
-
+    else if (document.getElementById("curTextType").innerHTML == "Pinyin") {
+        //Setting button text
+        document.getElementById("upTextType").innerHTML = "▲ Characters ▲";
+        document.getElementById("curTextType").innerHTML = "English";
+        document.getElementById("downTextType").innerHTML = "▼ Pinyin ▼";
+        //Setting flashcard text
+        document.getElementById("cardText").innerHTML = upCard("English");
     }
-    else if (document.getElementById("curTextType") == "Characters") {
-
+    else if (document.getElementById("curTextType").innerHTML == "English") {
+        //Setting button text
+        document.getElementById("upTextType").innerHTML = "▲ Pinyin ▲";
+        document.getElementById("curTextType").innerHTML = "Characters";
+        document.getElementById("downTextType").innerHTML = "▼ English ▼";
+        //Setting flashcard text
+        document.getElementById("cardText").innerHTML = upCard("Characters");
     }
     else {
         console.log('SOMETHING IS WRONG: You shouldnt be here')
     }
-    document.getElementById("cardText").innerHTML = "New Card";
 
 });
 
