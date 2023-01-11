b9908ffadacfe5e052b254bcc943b25e66c8edb6
hwmarsland <hwmarsland@wm.edu>
2023-01-11 13:48:48 -0500

    Up/Down buttons on flashcards fully functional

--- card_viewer.js
+++ card_viewer.js
@@ -1,6 +1,6 @@
-// import { nextCard } from "./card_list";
-// import { previousCard } from "./card_list";
-import { flipCard } from "./card_list";
+// import { nextCard } from "./card_list.js";
+// import { previousCard } from "./card_list.js";
+import { flipCard } from "./card_list.js";
 
 const upArrow = document.getElementById("upArrow")
 
@@ -41,6 +41,34 @@ const downArrow = document.getElementById("downArrow")
 
 downArrow.addEventListener('click', function handleDownClick() {
     console.log('Down arrow button clicked');
+    //Handling the visual changes
+    if (document.getElementById("curTextType").innerHTML == "Characters") {
+        //Setting button text
+        document.getElementById("upTextType").innerHTML = "▲ Characters ▲";
+        document.getElementById("curTextType").innerHTML = "English";
+        document.getElementById("downTextType").innerHTML = "▼ Pinyin ▼";
+        //Setting flashcard text
+        document.getElementById("cardText").innerHTML = flipCard("English");
+    }
+    else if (document.getElementById("curTextType").innerHTML == "Pinyin") {
+        //Setting button text
+        document.getElementById("upTextType").innerHTML = "▲ Pinyin ▲";
+        document.getElementById("curTextType").innerHTML = "Characters";
+        document.getElementById("downTextType").innerHTML = "▼ English ▼";
+        //Setting flashcard text
+        document.getElementById("cardText").innerHTML = flipCard("Characters");
+    }
+    else if (document.getElementById("curTextType").innerHTML == "English") {
+        //Setting button text
+        document.getElementById("upTextType").innerHTML = "▲ English ▲";
+        document.getElementById("curTextType").innerHTML = "Pinyin";
+        document.getElementById("downTextType").innerHTML = "▼ Characters ▼";
+        //Setting flashcard text
+        document.getElementById("cardText").innerHTML = flipCard("Pinyin");
+    }
+    else {
+        console.log('SOMETHING IS WRONG: You shouldnt be here')
+    }
 });
 
 
