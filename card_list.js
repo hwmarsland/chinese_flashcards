ab33951a79cf5da42a5b9264dba2c655f3de10e2
hwmarsland <hwmarsland@wm.edu>
2023-02-07 12:45:13 -0500

    Changed to allow TM to enter data

--- card_list.js
+++ card_list.js
@@ -1,18 +1,4 @@
-const sampleDeck = {    
-    name: "sampleDeck",
-    cards: [
-    {characters: '搬', pinyin: 'bān', english: 'to move'},
-    {characters: '但是', pinyin: 'dànshì', english: 'but'},
-    {characters: '必须', pinyin: 'bìxū', english: 'must'},
-    {characters: '房租', pinyin: 'fángzū', english: 'to rent'},
-    {characters: '楼上', pinyin: 'lóushàng', english: 'upstairs'},
-    {characters: '马上', pinyin: 'mǎshàng', english: 'immediately'},
-    {characters: '房东', pinyin: 'fángdōng', english: 'landlord'},
-    {characters: '房客', pinyin: 'fángkè', english: 'tentant'},
-    {characters: '以后', pinyin: 'yǐhòu', english: 'after'},
-    {characters: '好像', pinyin: 'hǎoxiàng', english: 'to seem like'},
-    ]
-}
+import { sampleDeck } from "./deck_storage.js";
 
 // Keeps track of the card being displayed
 var cardIndexPointer = 0;
