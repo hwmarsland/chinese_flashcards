2a999bfa0343e2bf10875fbd42a43d6e37fad34e
marslat16 <perlidae16@gmail.com>
2023-02-09 20:33:46 -0500

    Lesson 1 deck added

--- deck_storage.js
+++ deck_storage.js
@@ -14,4 +14,23 @@ export const sampleDeck = {
     ]
 }
 
-//Testing 123
+export const l1_101 = {
+    name: "Chinese 101: Lesson 1",
+    cards: [
+        {characters: '伱', pinyin: 'nǐ', english: 'you'},
+        {characters: '好', pinyin: 'hǎo', english: 'good, well'},
+        {characters: '是', pinyin: 'shì', english: 'to be, (affirmative answer) yes'},
+        {characters: '学生', pinyin: 'xuésheng', english: 'student'},
+        {characters: '学', pinyin: 'xué', english: 'to study, to learn'},
+        {characters: '吗', pinyin: 'ma', english: '(used at the end of a declarative sentence to transtorm it into a question)'},
+        {characters: '我 ', pinyin: 'wǒ', english: 'I, me'},
+        {characters: '呢', pinyin: 'ne', english: '(used at the end of an interrogative sentence)'},
+        {characters: '也', pinyin: 'yě', english: 'also'},
+        {characters: '他', pinyin: 'tā', english: 'he, him'},
+        {characters: '不', pinyin: 'bù', english: '(used to form a negative) not, no'},
+        {characters: '老师', pinyin: 'lǎoshī', english: 'teacher'},
+        {characters: '中文', pinyin: 'Zhōngwén', english: 'Chinese (language)'},
+        {characters: '她', pinyin: 'tā', english: 'she, her'},
+        {characters: '教授', pinyin: 'jiàoshòu', english: 'professor'},
+    ]
+}
