e024df9b26cafd1bbc0305425a94586ad7f0d155
marslat16 <perlidae16@gmail.com>
2023-02-10 19:20:01 -0500

    101 Lesson 2

--- deck_storage.js
+++ deck_storage.js
@@ -34,3 +34,25 @@ export const l1_101 = {
         {characters: '教授', pinyin: 'jiàoshòu', english: 'professor'},
     ]
 }
+
+export const l2_101 = {    
+    name: "Chinese 101: Lesson 2",
+    cards: [
+    {characters: '您', pinyin: 'nín', english: '(polite) you'},
+    {characters: '贵', pinyin: 'guì', english: 'noble, honored, expensive'},
+    {characters: '姓', pinyin: 'xìng', english: 'N. surname, family name; V. have as a surname'},
+    {characters: '请问', pinyin: 'qǐng wèn', english: 'may I ask'},
+    {characters: '请', pinyin: 'qǐng', english: '(polite) please'},
+    {characters: '问', pinyin: 'wèn', english: 'to ask'},
+    {characters: '的', pinyin: 'de', english: '(a structural particle used to show possession'},
+    {characters: '英文', pinyin: 'Yingwén', english: 'English (language)'},
+    {characters: '名字', pinyin: 'míngzi', english: 'name'},
+    {characters: '中文', pinyin: 'Zhōngwén', english: 'Chinese (language)'},
+    {characters: '叫', pinyin: 'jiào', english: 'to call, to be called'},
+    {characters: '什么', pinyin: 'shénme', english: 'what'},
+    {characters: '她', pinyin: 'tā', english: 'she, her'},
+    {characters: '谁', pinyin: 'shéi/shuí', english: 'who, whom'},
+    {characters: '同学', pinyin: 'tóngxué', english: 'classmate'},
+    {characters: '同', pinyin: 'tóng', english: 'same, similar'},
+    ]
+}
